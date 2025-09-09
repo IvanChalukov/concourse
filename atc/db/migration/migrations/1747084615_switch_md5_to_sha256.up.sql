1902ff0e5093c3cd5c8a1126893589ff8071b581
i346084 <ivan.chalakov@sap.com>
2025-09-09 12:15:58 +0300

    Validate it with 'COALESCE'

--- atc/db/migration/migrations/1747084615_switch_md5_to_sha256.up.sql
+++ atc/db/migration/migrations/1747084615_switch_md5_to_sha256.up.sql
@@ -53,7 +53,10 @@ WITH json_string_cte AS (
     SELECT 
         rcv.id,
         rcv.version_sha256 AS old_version_sha256,
-        '{' || string_agg('"' || kv.key || '":"' || kv.value || '"', ',' ORDER BY kv.key) || '}' AS json_string
+        COALESCE(
+            '{' || string_agg('"' || kv.key || '":"' || kv.value || '"', ',' ORDER BY kv.key) || '}',
+            '{}'
+        ) AS json_string
     FROM resource_config_versions rcv
     JOIN jsonb_each_text(rcv.version::jsonb) AS kv ON true
     WHERE jsonb_typeof(rcv.version::jsonb) = 'object'
